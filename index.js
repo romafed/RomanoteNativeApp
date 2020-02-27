c8c58088f79720ea20c4b732cbae7d64f7c54327
Roman <romankrvchk@gmil.com>
2020-02-27 11:38:35 +0200

    Connect redux store to the app

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 import 'react-native-gesture-handler';
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import {Provider} from 'react-redux';
+import store from './src/store/store';
+
+const AppWrapper = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => AppWrapper);
