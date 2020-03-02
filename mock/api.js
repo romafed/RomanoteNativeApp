841e250147677b6bbb89972c272786196ae0ba55
Roman <romankrvchk@gmil.com>
2020-03-02 13:35:35 +0200

    Create mock

---
+++ mock/api.js
@@ -0,0 +1,27 @@
+import { AsyncStorage } from 'react-native';
+
+export const createUserMock = async user => {
+  try {
+    const exist = await AsyncStorage.getItem(user.email);
+    if (exist) throw new Error('User is already exist');
+    await AsyncStorage.setItem(user.email, JSON.stringify(user));
+  } catch (ex) {
+    throw new Error(ex.message);
+  }
+};
+
+export const logInUserMock = async ({ email, password }) => {
+  try {
+    const exist = await AsyncStorage.getItem(email);
+    if (!exist) throw new Error('Email or password is incorrect');
+
+    const user = JSON.parse(exist);
+    const match = user.password === password;
+    if (!match) throw new Error('Email or password is incorrect');
+
+    delete user.password;
+    return JSON.stringify(user);
+  } catch (ex) {
+    throw new Error('ex.message');
+  }
+};
